fcb57303f5dc9686f1ef6ff1814c9401afb1f4db
Maxim <130044952+gera404@users.noreply.github.com>
2023-11-03 21:31:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function colorCh1(e){
+    d4.style.background = `greenyellow`
+    
+}
+function colorCh2(e){
+    d4.style.background = `yellow`
+    
+}
+function colorCh3(e){
+    d4.style.background = `red`
+}
+function button1() {
+    newWind = window.open('https://journal.top-academy.ru/ru/main/homework/page/index')
+}
+function button2() {
+    newWind.close()
+}
